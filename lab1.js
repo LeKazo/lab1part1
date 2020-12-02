c15260f26cbd83d13337da75b5d8ffbe26f3739e
LeKazo <70152390+LeKazo@users.noreply.github.com>
2020-12-02 11:25:20 -0500

    Lab1part1

---
+++ lab1.js
@@ -0,0 +1,18 @@
+var secondsInAMinute = 60;
+var minutesInAHour = 60;
+var hoursInADay = 24;
+var daysInAYear = 365;
+
+var secondsInAHour = secondsInAMinute * minutesInAHour;
+document.write("The Number of seconds in a day are " + secondsInAHour);
+document.write("<br>");
+
+var secondsInADay = secondsInAHour * hoursInADay;
+document.write("The Number of seconds in a day are " + secondsInADay);
+document.write("<br>");
+
+var secondsInAYear = secondsInADay * daysInAYear;
+document.write("The Number of seconds in a day are " + secondsInAYear);
+document.write("<br>");
+document.write("<br>");
+document.write("<Created by Q>");
